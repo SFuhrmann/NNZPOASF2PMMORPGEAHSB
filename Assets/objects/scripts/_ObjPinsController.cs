c62bb117f3f7053643605d97a8935f728940c242
Florian <mail@florianhaase.de>
2014-03-28 22:53:24 +0100

    + Items + Script

---
+++ Assets/objects/scripts/_ObjPinsController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _ObjPinsController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
