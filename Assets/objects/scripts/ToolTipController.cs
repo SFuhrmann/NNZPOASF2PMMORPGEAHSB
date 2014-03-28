a24fac2eb868a807ddb5365e6067ba3fb06e9312
Florian <mail@florianhaase.de>
2014-03-29 00:12:17 +0100

    +tooltipcontroller

---
+++ Assets/objects/scripts/ToolTipController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolTipController : MonoBehaviour {
+
+	private string instanceToolTip;
+	internal static ToolTipController instance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void setToolTip(string toolTip) {
+		instance.instanceToolTip = toolTip;
+	}
+}
