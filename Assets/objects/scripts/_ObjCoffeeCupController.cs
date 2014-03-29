ad8a17cce5e6f62d12265e5171882e9fdda695ce
stfuhrmann <fumse@live.de>
2014-03-29 05:14:18 +0100

    yeah coffee

--- Assets/objects/scripts/_ObjCoffeeCupController.cs
+++ Assets/objects/scripts/_ObjCoffeeCupController.cs
@@ -10,22 +10,12 @@ public class _ObjCoffeeCupController : ObjController {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        base.Update();
 	}
-	void OnSwipeUp() { 
-		//do something sweet
-		Debug.Log ("swipe up");
-	}
-	void OnSwipeRight() { 
-		//do something awesome
-		Debug.Log ("swipe right");
-	}
-	void OnSwipeDown() { 
-		//do something great
-		Debug.Log ("swipe down");
-	}
-	void OnSwipeLeft() { 
-		//do something nice
-		Debug.Log ("swipe left");
-	}
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (dragging || !draggingReady) return;
+        print("collides coffeecup");
+    }
 }
